8cbee43f257ec7648eca4ff349bf4bbbe281ac5f
1onlinesolution <team@damianos.io>
2021-06-07 12:13:45 +0100

    more unit tests

--- test/session.spec.js
+++ test/session.spec.js
@@ -1,13 +1,20 @@
 const assert = require('assert');
+const session = require('../lib/session');
 
 describe('session', () => {
-  it('Check the type', () => {
-    const session = require('../lib/session')({
+  it('"session" is a function', () => {
+    const mySession = session({
       secret: 'aaa',
-      name: 'name', // <-- a less generic name for the session id
       resave: false,
       saveUninitialized: false,
     });
-    assert(typeof session === 'function');
+    assert(typeof mySession === 'function');
   });
+
+  it('"login" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.login === 'function'));
+  it('"isLoggedIn" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.isLoggedIn === 'function'));
+  it('"isGuest" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.isGuest === 'function'));
+  it('"isFresh" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.isFresh === 'function'));
+  it('"saveAsync" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.saveAsync === 'function'));
+  it('"reloadAsync" is a function of "session" (prototype)', () => assert(typeof session.Session.prototype.reloadAsync === 'function'));
 });
